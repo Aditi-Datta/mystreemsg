04e97d1a3ad01f651383ed7cf2c8bfff04c68968
Aditi <aditidatta373@gmail.com>
2024-06-25 19:23:08 +0600

    verification email

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -6,3 +6,13 @@ export const messageSchema = z.object({
    .min(10, {message: 'Content must be at least of 10 characters'})
   .max(300, {message: 'Content must be no longer than 300 characters'} )
 })
+
+
+
+
+
+
+
+
+
+
